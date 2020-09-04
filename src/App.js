8487d8c61042496a25dfe50a83fea846e7010eeb
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-04 09:26:10 +0530

    axios created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Video from './Video';
 import axios from './axios';
 
 function App() {
+  const [videos,setVideos] =useState([])
+
+  useState(()=>{
+    async function fetchPosts(){
+      const response = await axios.get('/v2/posts');
+      setVideos(response.data);
+      return response;
+    }
+    fetchPosts();
+  })
   return (
     <div className="App">
       <div className='app_videos'>
