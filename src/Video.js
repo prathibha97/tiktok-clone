46e42953bdab54cb0889199b11a13efdea62575b
Prathibha Ratnayake <prsthibha@gmail.com>
2020-09-04 09:13:57 +0530

    video.js configured

---
+++ src/Video.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
+
+function Video({ url, channel, description, likes, song, shares, messages }) {
+  const videoRef = useState(null);
+  const [playing, setPlaying] = useState(false);
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef.current.puase();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+  return (
+    <div className='video'>
+      <video
+        onClick={handleVideoPress}
+        className='video_player'
+        loop
+        ref={videoRef}
+        src={url}
+      ></video>
+      <VideoFooter/>
+      <VideoSidebar/>
+    </div>
+  );
+}
+
+export default Video;
